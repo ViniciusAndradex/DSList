69990d2c672a538d0ad728aed5c05ce4f474a73c
ViniciusAndradeX <viniciusandradeprog@gmail.com>
2023-07-10 23:03:29 -0300

    Game seed.

--- src/main/java/com/andlab/dslist/entities/Game.java
+++ src/main/java/com/andlab/dslist/entities/Game.java
@@ -14,19 +14,23 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){super();}
 
-    public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -64,12 +68,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
     }
 
     public String getImgUrl() {
@@ -92,8 +104,8 @@ public class Game {
         return longDescription;
     }
 
-    public void setLongDescription(String longDescription) {
-        this.longDescription = longDescription;
+    public Double getScore() {
+        return score;
     }
 
     @Override
@@ -102,13 +114,13 @@ public class Game {
         if (o == null || getClass() != o.getClass()) return false;
         Game game = (Game) o;
         return Objects.equals(id, game.id) && Objects.equals(title, game.title) && Objects.equals(genre, game.genre) &&
-                Objects.equals(platform, game.platform) && Objects.equals(imgUrl, game.imgUrl) &&
+                Objects.equals(platforms, game.platforms) && Objects.equals(imgUrl, game.imgUrl) &&
                 Objects.equals(shortDescription, game.shortDescription) && Objects.equals(longDescription, game.longDescription);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, genre, platform, imgUrl, shortDescription, longDescription);
+        return Objects.hash(id, title, genre, platforms, imgUrl, shortDescription, longDescription);
     }
 
 
